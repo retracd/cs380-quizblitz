64d9205d2b3cb958aa6add9d713a1a1983820b00
retracd <29313219+retracd@users.noreply.github.com>
2026-03-09 09:23:23 +0100

    Add StartScreen component

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="start-screen">
+    <h1>QuizBlitz</h1>
+    <p>Test your tech knowledge in this fast-paced trivia game!</p>
+    <button @click="emitStart">Play</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartScreen',
+  emits: ['start'],
+  methods: {
+    emitStart() {
+      this.$emit('start');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.start-screen {
+  padding: 2rem;
+}
+button {
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  cursor: pointer;
+}
+</style>
